842f14631b5ec83d11c78c7cd8b75ef1615a5b99
rafael cavalcante chaves <rafaelchavesaec@gmail.com>
2023-11-13 15:56:12 -0300

    Atualização do Algoritmo Genético

--- genetic.cpp
+++ genetic.cpp
@@ -1,18 +1,27 @@
 #include <bits/stdc++.h>
+#define MUTATION_RATE 0.03
+#define NEW_POPULATION_RATE 0.75
 #define POPULATION_SIZE 1000 
-#define MAXIT 100000
-#define TIMELIMIT 10
+#define TIMELIMIT 20
 #define AWARD 250
+#define TIME_NOW timediff_in_sec(start, clock())
 
 using namespace std;
 
 typedef struct Solution {
 	vector<int> seq_a, seq_b;
-	int best_value = INT_MIN, best_start, best_end;
+	int best_value, best_start, best_end;
+	double created_at;
+	Solution(){
+		this->created_at = 0;
+		this->best_value = INT_MIN;
+	}
 } solution;
 
 int size_a, size_b;
 auto random_generator = default_random_engine(clock());
+clock_t start; 
+
 vector<vector<int>> BackwardEdges; // ai <- bj
 vector<vector<int>> FowardEdges; // ai -> bj
 vector<solution> population(POPULATION_SIZE);
@@ -21,39 +30,50 @@ void crossover_operation(vector<int> &a, vector<int> &b, vector<int> &merged, in
 void solution_build(solution &sol);
 bool solution_comp(solution &a, solution &b);
 solution solution_crossover(solution &a, solution &b);
+void solution_mutation(solution &sol);
 void solution_objective_function(solution &sol);
 void solution_print(solution &sol);
 void read_input(void);
-int timediff_in_sec(clock_t start, clock_t end);
+double timediff_in_sec(clock_t start, clock_t end);
 
 int main(){
 	read_input();
 	solution best;
-	int it = 0;
 	vector<int> reproduction_order(POPULATION_SIZE);
 	for(int i = 0; i < POPULATION_SIZE; i++) reproduction_order[i] = i;
 	for(auto &sol: population){
 		solution_build(sol);
 		solution_objective_function(sol);
 	}
-	clock_t start = clock();
-	while(it++ <= MAXIT){
+	start = clock();
+	int time_next_step = 1;
+	while(TIME_NOW - best.created_at < TIMELIMIT){
+		if(int(TIME_NOW) == time_next_step){
+			printf("Melhor: %d, Primeiro: %d, Ultimo: %d\n", best.best_value, population.front().best_value, population.back().best_value);
+			time_next_step++;
+		}
+		for(int i = 0; i < POPULATION_SIZE*MUTATION_RATE; i++){
+			int idx = random_generator() % POPULATION_SIZE;
+			solution_mutation(population[idx]);
+		}
 		shuffle(reproduction_order.begin(), reproduction_order.end(), random_generator);
-		for(int i = 0; i < POPULATION_SIZE; i+=2) {
+		for(int i = 0; i < POPULATION_SIZE; i++) {
 			int a = reproduction_order[i];
-			int b = reproduction_order[i+1];
+			int b = reproduction_order[(i+1)%POPULATION_SIZE];
 			population.push_back(solution_crossover(population[a], population[b]));
 		}
 		sort(population.begin(), population.end(), solution_comp);
-		while(population.size() > POPULATION_SIZE) population.pop_back();
-		//cout << population.front().best_value <<  " " << population.back().best_value << endl;
-		if(solution_comp(population.front(), best)){
-			it = 0;
-			best = population.front();
+		while(population.size() > POPULATION_SIZE*NEW_POPULATION_RATE) population.pop_back();
+		while(population.size() < POPULATION_SIZE){
+			solution sol;
+			solution_build(sol);
+			solution_objective_function(sol);
+			population.push_back(sol);
 		}
-		
+		if(solution_comp(population.front(), best))
+			best = population.front();
 	}
-	solution_print(best);
+	printf("total_time: %.3lf - best: %d - created_at: %.3lf s\n", timediff_in_sec(start, clock()), best.best_value, best.created_at);
 	return 0;
 }
 
@@ -79,6 +99,7 @@ void solution_build(solution &sol){
 	sol.seq_b.resize(size_b);
 	for(int i = 0; i < size_b; i++) sol.seq_b[i] = i;
 	shuffle(sol.seq_b.begin(), sol.seq_b.end(), random_generator); 
+	sol.created_at = timediff_in_sec(start, clock());
 }
 
 bool solution_comp(solution &a, solution &b){
@@ -98,9 +119,20 @@ solution solution_crossover(solution &a, solution &b){
 	int cut_start_b = random_generator() % (size_b/2);
 	int cut_len_b = max((int) (random_generator() % (size_b - cut_start_b)), 2*size_b/5);
 	crossover_operation(a.seq_b, b.seq_b, sol.seq_b, cut_start_b, cut_len_b);
-
+	sol.created_at = TIME_NOW;
 	solution_objective_function(sol);
 	return sol;
+}
+
+void solution_mutation(solution &sol){
+	int idx0, idx1, idx2,  edge = min(size_a,  size_b);
+	idx0 = random_generator() % edge;
+	idx1 = random_generator() % edge;
+	idx2 = random_generator() % edge;
+	swap(sol.seq_a[idx0], sol.seq_a[idx1]);
+	swap(sol.seq_b[idx1], sol.seq_b[idx2]);
+	sol.created_at = TIME_NOW;
+	solution_objective_function(sol);
 }
 
 void solution_objective_function(solution &sol){
@@ -148,6 +180,6 @@ void read_input(void){
 	for(auto &row: BackwardEdges) for(auto &edge: row) cin >> edge;
 }
 
-int timediff_in_sec(clock_t start, clock_t end){
-	return (end - start)/ CLOCKS_PER_SEC;
+double timediff_in_sec(clock_t start, clock_t end){
+	return (1.0*end - start)/ CLOCKS_PER_SEC;
 }
